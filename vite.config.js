0a7a845ea94f636722de4a71216c460d424745a7
Nuthfih <asatriaraghib@gmail.com>
2025-07-30 19:27:39 +0700

    fix: building css and js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/styles.css', 'resources/js/scrollreveal.min.js', 'resources/js/main.js', 'resources/css/profile.css', 'resources/js/profile.js', 'resources/css/profile.css'],
             refresh: true,
         }),
         tailwindcss(),
