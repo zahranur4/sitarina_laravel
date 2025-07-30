b5fe6705bb91a6b9102d6b478e784e99dca23333
Nuthfih <asatriaraghib@gmail.com>
2025-07-30 19:10:22 +0700

    feat: adding home controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\Product;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return view('homepage/index', compact('products'));
+    }
+}
